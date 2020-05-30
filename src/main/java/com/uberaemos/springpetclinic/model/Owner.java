e70f6a3095fddae961196db31716ea74827994d5
Anıl Sakaryalı <aanilsakaryali@gmail.com>
2020-05-31 00:54:28 +0300

    Adds POJO data model.

---
+++ src/main/java/com/uberaemos/springpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.uberaemos.springpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/uberaemos/springpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.uberaemos.springpetclinic.model;
+
+public class Vet extends Person {
+}
