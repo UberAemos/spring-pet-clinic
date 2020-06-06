0a9980054c561d2c92e7a768491957cdf4c472f0
Anıl Sakaryalı <aanilsakaryali@gmail.com>
2020-06-06 14:36:59 +0300

    Refactors AbstractMapService for automatic id generation.

--- pet-clinic-web/src/main/java/com/uberaemos/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/uberaemos/springpetclinic/bootstrap/DataLoader.java
@@ -21,13 +21,11 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Owner");
         owner1.setLastName("One");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Owner");
         owner2.setLastName("Two");
         ownerService.save(owner2);
@@ -35,13 +33,11 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Vet");
         vet1.setLastName("One");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Vet");
         vet2.setLastName("Two");
         vetService.save(vet2);
