fe3c59f4f6ade4a0cf630c9f2b7b57894419460d
Anıl Sakaryalı <aanilsakaryali@gmail.com>
2020-06-06 16:19:52 +0300

    Adds a second mock properties file, and inserts both resources to config class.

--- pet-clinic-data/src/main/java/com/uberaemos/springpetclinic/config/PropertyConfig.java
+++ pet-clinic-data/src/main/java/com/uberaemos/springpetclinic/config/PropertyConfig.java
@@ -5,10 +5,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+@PropertySources({
+        @PropertySource("classpath:data-source-one.properties"),
+        @PropertySource("classpath:data-source-two.properties")
+})
 public class PropertyConfig {
 
     @Value("${data.username}")
