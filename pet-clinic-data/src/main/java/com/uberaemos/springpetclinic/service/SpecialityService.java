21f92c825e9e18c980ec9a225399e8019c8fbb36
Anıl Sakaryalı <aanilsakaryali@gmail.com>
2020-06-15 10:36:30 +0300

    Adds service interface and implementation for Speciality

---
+++ pet-clinic-data/src/main/java/com/uberaemos/springpetclinic/service/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.uberaemos.springpetclinic.service;
+
+import com.uberaemos.springpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
