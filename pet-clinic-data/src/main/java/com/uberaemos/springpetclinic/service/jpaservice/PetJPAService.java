1ae5402f6635df13de73ae691ba33145bbfd7a7f
Anıl Sakaryalı <aanilsakaryali@gmail.com>
2020-06-15 15:36:22 +0300

    Adds jpa implementation for Pet.

---
+++ pet-clinic-data/src/main/java/com/uberaemos/springpetclinic/service/jpaservice/PetJPAService.java
@@ -0,0 +1,48 @@
+package com.uberaemos.springpetclinic.service.jpaservice;
+
+import com.uberaemos.springpetclinic.model.Pet;
+import com.uberaemos.springpetclinic.repository.PetRepository;
+import com.uberaemos.springpetclinic.service.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class PetJPAService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetJPAService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
